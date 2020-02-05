8c08fac57dece6f93f6e7c9d209e2a0f8bf7ef58
edpchinthana <edpchinthana@outlook.com>
2020-02-05 14:18:44 +0530

    Completed till searching techniuqes

--- BinarySearch/main.c
+++ BinarySearch/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int size;
 int searching(int a[], int key){
     int start = 0;
@@ -25,7 +26,7 @@ int searching(int a[], int key){
 int main()
 {
     int a[10]={1,5,7,9,11,15,23,25,56,60};
-    int key=15;
+    int key=0;
     size=sizeof(a)/sizeof(a[0]);
     int result = searching(a,key);
     printf("\n%d",result);
